0a8d7206adda55e648d4abf329366c8d7384453c
Dhananjay <ddhananjay684@gmail.com>
2026-02-03 22:28:22 +0530

    EXpert profile created

--- career-talk-mobile/src/App.jsx
+++ career-talk-mobile/src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Sidebar from "./components/Sidebar";
 import SearchExperts from "./pages/expert/SearchExperts";
 import RecommendedExperts from "./pages/expert/RecommendedExperts";
+import ExpertSinglePage from "./pages/expert/ExpertSinglePage";
 
 function App() {
   return (
@@ -13,11 +14,12 @@ function App() {
           <Routes>
             <Route path="/" element={<SearchExperts />} />
             <Route path="/recommended" element={<RecommendedExperts />} />
+            <Route path="/expert/:id" element={<ExpertSinglePage />} />
           </Routes>
         </div>
       </div>
     </BrowserRouter>
-  );
+);
 }
 
 export default App;
